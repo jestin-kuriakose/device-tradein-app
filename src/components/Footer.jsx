8246f1c16c56d43799caf8b716689538255bc707
jestin-kuriakose <jestin.kuriakose@teslaelectronics.ca>
2022-10-28 06:08:55 -0400

    made changes to styling in Tradein form component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
         <footer className="text-muted py-5">
             <div className="container">
                 <p className="float-end mb-1">
-                <a href="/">Back to top</a>
                 </p>
                 <p className="mb-1">Design by &copy; Jestin Kuriakose</p>
             </div>
--- src/components/TradeinForm.jsx
+++ src/components/TradeinForm.jsx
@@ -81,11 +81,11 @@ const navigate = useNavigate()
     <div className='container p-3'>
         <form className='col g-3 justify-content-lg-center'>
         
-            <h3>What Is The Model Of Your Device Being Evaluated?</h3>
-            <p>Select the details of your device model from the following options.</p>
+            <h3 className='display-6 text-center'>What Is The Model Of Your Device Being Evaluated?</h3>
+            <p className='lead text-center'>Select the details of your device model from the following options.</p>
             <div className="mb-3 row-auto lg-auto justify-content-center">
-                <div>
-                    <label className="form-label">Select your phone</label>
+                <div className='col-md-6 mx-auto my-0'>
+                    <label className="form-label lead">Select your phone</label>
                     <select className='form-select' defaultValue={'DEFAULT'} onChange={handleChange}>
                         <option disabled value={'DEFAULT'}> -- select an option -- </option>
                         {phoneData?.map((data)=>(
@@ -94,8 +94,8 @@ const navigate = useNavigate()
                     </select>
                 </div>
 
-                <div>
-                    <label className="form-label">Storage</label>
+                <div className='col-md-6 mx-auto my-0'>
+                    <label className="form-label lead">Storage</label>
                     <select className='form-select' defaultValue={'DEFAULT'} onChange={(e)=>setInputData({...inputData, size: e.target.value})}>
                         <option disabled value={'DEFAULT'}> -- select an option -- </option>
                         {selectedPhoneData?.size?.map((d)=>(
@@ -104,8 +104,8 @@ const navigate = useNavigate()
                     </select>
                 </div>
 
-                <div>
-                    <label className="form-label">Color</label>
+                <div className='col-md-6 mx-auto my-0'>
+                    <label className="form-label lead">Color</label>
                     <select className='form-select' defaultValue={'DEFAULT'} onChange={(e)=>setInputData({...inputData, color: e.target.value})}>
                     <option disabled value={'DEFAULT'}> -- select an option -- </option>
                         {selectedPhoneData?.color?.map((d)=>(
@@ -114,51 +114,51 @@ const navigate = useNavigate()
                     </select>
                 </div>
 
-            <h3 className='mt-5 text-center'>What Is the Condition of Your Device Being Evaluated?</h3>
-                <div className='row shadow-lg p-3 my-5 bg-white rounded'>
-                    <h5 className='row-auto'>What is the physical condition of the device?</h5>
+            <h3 className='mt-5 text-center display-6'>What Is the Condition of Your Device Being Evaluated?</h3>
+                <div className='row shadow-lg p-3 my-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>What is the physical condition of the device?</h5>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.condition === "VERY GOOD" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'VERY GOOD'} onClick={(e)=>setInputData({...inputData, condition: e.target.value})}>VERY GOOD</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.condition === "GOOD" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'GOOD'} onClick={(e)=>setInputData({...inputData, condition: e.target.value})}>GOOD</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.condition === "ACCEPTABLE" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'ACCEPTABLE'} onClick={(e)=>setInputData({...inputData, condition: e.target.value})}>ACCEPTABLE</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.condition === "BROKEN" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'BROKEN'} onClick={(e)=>setInputData({...inputData, condition: e.target.value})}>BROKEN</button>
                 </div>
 
-                <div className='row shadow-lg p-3 mb-5 bg-white rounded'>
-                    <h5 className='row-auto'>Does your phone power on when charged or plugged in?</h5>
+                <div className='row shadow-lg p-3 mb-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>Does your phone power on when charged or plugged in?</h5>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.power_on === "YES" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'YES'} onClick={(e)=>setInputData({...inputData, power_on: e.target.value})}>YES</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.power_on === "NO" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NO'} onClick={(e)=>setInputData({...inputData, power_on: e.target.value})}>NO</button>
                 </div>
 
-                <div className='row shadow-lg p-3 mb-5 bg-white rounded'>
-                    <h5 className='row-auto'>Is your screen cracked, severely scratched or chipped?</h5>
+                <div className='row shadow-lg p-3 mb-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>Is your screen cracked, severely scratched or chipped?</h5>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.screen_con === "YES" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'YES'} onClick={(e)=>setInputData({...inputData, screen_con: e.target.value})}>YES</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.screen_con === "NO" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NO'} onClick={(e)=>setInputData({...inputData, screen_con: e.target.value})}>NO</button>
                 </div>
 
-                <div className='row shadow-lg p-3 mb-5 bg-white rounded'>
-                    <h5 className='row-auto'>Is your back glass broken, severely scratched or chipped?</h5>
+                <div className='row shadow-lg p-3 mb-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>Is your back glass broken, severely scratched or chipped?</h5>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.back_con === "YES" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'YES'} onClick={(e)=>setInputData({...inputData, back_con: e.target.value})}>YES</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.back_con === "NO" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NO'} onClick={(e)=>setInputData({...inputData, back_con: e.target.value})}>NO</button>
                 </div>
 
-                <div className='row shadow-lg p-3 mb-5 bg-white rounded'>
-                    <h5 className='row-auto'>Is your phone bent?</h5>
+                <div className='row shadow-lg p-3 mb-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>Is your phone bent?</h5>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.bent === "YES" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'YES'} onClick={(e)=>setInputData({...inputData, bent: e.target.value})}>YES</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.bent === "NO" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NO'} onClick={(e)=>setInputData({...inputData, bent: e.target.value})}>NO</button>
                 </div>
 
-                <div className='row shadow-lg p-3 mb-5 bg-white rounded'>
-                    <h5 className='row-auto'>Do ALL of the following function properly?</h5>
-                    <p>This includes: Cameras, Speakers, Home Button, Power Button, Volume Button, Microphone, and Battery.</p>
+                <div className='row shadow-lg p-3 mb-5 bg-white rounded justify-content-center text-center'>
+                    <h5 className='row-auto lead'>Do ALL of the following function properly?</h5>
+                    <p className='lead'>This includes: Cameras, Speakers, Home Button, Power Button, Volume Button, Microphone, and Battery.</p>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.misc === "YES" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'YES'} onClick={(e)=>setInputData({...inputData, misc: e.target.value})}>YES</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.misc === "NO" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NO'} onClick={(e)=>setInputData({...inputData, misc: e.target.value})}>NO</button>
                     <button className={"btn col-auto lg-auto m-3 " + (inputData.misc === "NOT SURE" ? "btn-outline-secondary":"btn-secondary")} type='button' value={'NOT SURE'} onClick={(e)=>setInputData({...inputData, misc: e.target.value})}>NOT SURE</button>
                 </div>
-                {missingOptions && <p style={{textAlign: "center", color: "Red"}}>All the above fields are mandatory, Please select all the options to continue</p>}
+                {missingOptions && <p style={{textAlign: "center", color: "Red"}}>All the above fields are mandatory, Please make selection to all the above options to continue</p>}
             </div>
 
             <div className='row justify-content-center'>
-                <button type="submit" className="btn col-lg-2 btn-primary row-auto lg-auto justify-content-center" onClick={handleSubmit}>GET A QUOTE</button>
+                <button type="submit" className="btn btn-lg col-lg-2 btn-dark row-auto lg-auto justify-content-center" onClick={handleSubmit}>GET A QUOTE</button>
             </div>
 
         </form>
